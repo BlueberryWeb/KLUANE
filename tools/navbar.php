400a88b5c4cd3586584eeeed725034607da35adf
BlueberryWeb <blueberryweb7@gmail.com>
2022-04-29 12:54:52 -0500

    actualización pt1

--- tools/navbar.php
+++
@@ -1,8 +0,0 @@
-<section class="showcase" id="showcase">
-        <header class="navbar navbar-expand-lg">
-            <a class="navbar-brand">
-                <img src="img/Logo-KLUANE-Bco.gif" alt="KLUANE DRILLING LTD" class="img-fluid" id="logo-header"/></a>
-                <button class="btn btn-outline " onclick="menu()">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-        </header>
